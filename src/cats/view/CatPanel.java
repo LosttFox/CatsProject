f2ac979c9782368af50d40a024a7f7ad8a29c896
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-02-16 10:16:33 -0700

    created basic panel stub

--- src/cats/view/CatPanel.java
+++ src/cats/view/CatPanel.java
@@ -1,6 +1,34 @@
 package cats.view;
 
-public class CatPanel
-{
+import javax.swing.JPanel;
+import cats.controller.Controller;
 
+public class CatPanel extends JPanel
+{
+	private Controller app;
+	
+	public CatPanel(Controller app)
+	{
+		super();
+		this.app = app;
+		
+		setupPanel();
+		setupListeners();
+		setupLayout();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
 }
