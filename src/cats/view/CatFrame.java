4791b0fad8b8dfc795aac0f82d850049642b4518
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-02-16 10:12:53 -0700

    created basic frame stub

--- src/cats/view/CatFrame.java
+++ src/cats/view/CatFrame.java
@@ -1,6 +1,28 @@
 package cats.view;
 
-public class CatFrame
-{
+import javax.swing.JFrame;
 
+import cats.controller.Controller;
+
+public class CatFrame extends JFrame
+{
+	private Controller app;
+	
+	public CatFrame(Controller app)
+	{
+		super();
+		this.app = app;
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("Cats");
+		this.setSize(1024, 768);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		this.setVisible(true);
+	}
 }
