b9c296bf27f3b207d80589a6ae47bdaab9dc3b7c
LostFox <51424369+LosttFox@users.noreply.github.com>
2024-03-15 11:45:12 -0600

    added CatFrame to the Controller

--- src/cats/controller/Controller.java
+++ src/cats/controller/Controller.java
@@ -1,14 +1,17 @@
 package cats.controller;
 
 import cats.model.*;
+import cats.view.CatFrame;
 
 public class Controller 
 {
 	private ShelterCats cats;
+	private CatFrame window;
 	
 	public Controller()
 	{
 		this.cats = new ShelterCats();
+		this.window = new CatFrame(this);
 	}
 	
 	public void start()
